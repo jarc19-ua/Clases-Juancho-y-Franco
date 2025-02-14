77293dbccecb11d351ad131668ec7eada8759938
Franco <fragonqui@alu.edu.gva.es>
2025-02-14 16:29:45 +0100

    Modificadas la funcion addTeam

--- Prácticas/P2/Prac1/prac1.cc
+++ Prácticas/P2/Prac1/prac1.cc
@@ -3,22 +3,24 @@
 #include <cstring> // Para strcpy(), strcat() y strcmp()
 
 using namespace std;
-int ID=0;
-const int kTEAMNAME=40; // Máximo tamaño del nombre de un equipo
-const int kPLAYERNAME=50; // Máximo tamaño del nombre de un jugador
-const int kPLAYERS=5; // Número de jugadores por equipo
-const int kMAXTEAMS=20; // Número máximo de equipos
-const int kMINTEAMS=2; // Número mínimo de equipos
+int ID = 0;
+const int kTEAMNAME = 40;   // Máximo tamaño del nombre de un equipo
+const int kPLAYERNAME = 50; // Máximo tamaño del nombre de un jugador
+const int kPLAYERS = 5;     // Número de jugadores por equipo
+const int kMAXTEAMS = 20;   // Número máximo de equipos
+const int kMINTEAMS = 2;    // Número mínimo de equipos
 
 // Registro para los jugadores
-struct Player{
+struct Player
+{
     char name[kPLAYERNAME];
     unsigned int goals;
     bool best;
 };
 
 // Registro para los equipos
-struct Team{
+struct Team
+{
     unsigned int id;
     char name[kTEAMNAME];
     unsigned int wins;
@@ -29,7 +31,8 @@ struct Team{
 };
 
 // Tipos de error posibles
-enum Error{
+enum Error
+{
     ERR_EMPTY,
     ERR_MAX_TEAMS,
     ERR_NO_LEAGUE,
@@ -43,21 +46,30 @@ enum Error{
 e: tipo de error a mostrar
 return: nada
 */
-void error(Error e){
-    switch(e){
-        case ERR_EMPTY: cout << "ERROR: empty string" << endl;
-            break;
-        case ERR_MAX_TEAMS: cout << "ERROR: maximum number of teams reached" << endl;
-            break;
-        case ERR_NO_LEAGUE: cout << "ERROR: there are no standings" << endl;
-            break;
-        case ERR_NO_TEAMS: cout << "ERROR: there are no teams" << endl;
-            break;
-        case ERR_NOT_EXIST: cout << "ERROR: team does not exist" << endl;
-            break;
-        case ERR_NUM_TEAMS: cout << "ERROR: wrong number of teams" << endl;
-            break;
-        case ERR_OPTION: cout << "ERROR: wrong option" << endl;
+void error(Error e)
+{
+    switch (e)
+    {
+    case ERR_EMPTY:
+        cout << "ERROR: empty string" << endl;
+        break;
+    case ERR_MAX_TEAMS:
+        cout << "ERROR: maximum number of teams reached" << endl;
+        break;
+    case ERR_NO_LEAGUE:
+        cout << "ERROR: there are no standings" << endl;
+        break;
+    case ERR_NO_TEAMS:
+        cout << "ERROR: there are no teams" << endl;
+        break;
+    case ERR_NOT_EXIST:
+        cout << "ERROR: team does not exist" << endl;
+        break;
+    case ERR_NUM_TEAMS:
+        cout << "ERROR: wrong number of teams" << endl;
+        break;
+    case ERR_OPTION:
+        cout << "ERROR: wrong option" << endl;
     }
 }
 
@@ -65,13 +77,15 @@ void error(Error e){
 team: equipo para el que vamos a simular los goles marcados
 return: número de goles totales del equipo
 */
-int simulateGoals(Team &team){
-    int goals=0;
-    
-    for(int i=0;i<kPLAYERS;i++){
-        int goal=rand()%2; // Genera 0 o 1 (máximo un gol por robot en cada partido)
-        team.players[i].goals+=goal;
-        goals+=goal;
+int simulateGoals(Team &team)
+{
+    int goals = 0;
+
+    for (int i = 0; i < kPLAYERS; i++)
+    {
+        int goal = rand() % 2; // Genera 0 o 1 (máximo un gol por robot en cada partido)
+        team.players[i].goals += goal;
+        goals += goal;
     }
 
     return goals;
@@ -80,7 +94,8 @@ int simulateGoals(Team &team){
 /* Función que muestra el menú de opciones
 return: nada
 */
-void showMenu(){
+void showMenu()
+{
     cout << "1- Add team" << endl
          << "2- Add all teams" << endl
          << "3- Delete team" << endl
@@ -91,84 +106,87 @@ void showMenu(){
          << "q- Quit" << endl
          << "Option: ";
 }
-Team addTeam(){
-    int numeroActualEquipos=5;//no se como mirar el número de equipos
+Team addTeam()
+{
+    int numeroActualEquipos = 5; // no se como mirar el número maximo de equipos
     Team equipo;
-    if(numeroActualEquipos>kMAXTEAMS){
+
+    if (ID > kMAXTEAMS)
+    {
         error(ERR_MAX_TEAMS);
     }
-    else{
-        cout<<"Enter team name: ";
-        char nombre[kTEAMNAME];
-        string nombreEquipo;
-        cin>>nombreEquipo;
-        equipo.id=ID;
-        ID++;
-
-
-
-//MODIFICACION
-        equipo.name=nombre;//ver porque da fallo
+    else
+    {
+        string nombreEquipo = "";
+        cout << "Enter team name: ";
+        cin >> nombreEquipo;
         
+        equipo.id = ID;
+        ID++;
+       
+        if ( (nombreEquipo.length() > kTEAMNAME) || (nombreEquipo.length() == 0))
+        {// Se pone el nombre por defecto
+            nombreEquipo = "Team_"+equipo.id;
+        }
+
+        // O bien se asigna el nombre por defecto o bien un nombre valido introducido por el usuario
         for (int j = 0; j < nombreEquipo.length(); j++)
         {
-            equipo.name[j]=nombreEquipo[j];
+            equipo.name[j] = nombreEquipo[j];
         }
-        
-        
-        
-        equipo.wins=0;
-        equipo.losses=0;
-        equipo.draws=0;
-        equipo.points=0;
 
-        for(int i=0;i<5;i++){
-            Player jugador;
-            string name=equipo.name;
-
-
-
-
-
-
-
-
-
-
-//MODIFICACION
-            char nombre[20];
-            //char nombre[20]=name;//ver porque da fallo
-            
-            
-            for(int j=0;j<name.length();j++)
-            nombre[i]=name[i];
-            
+/* Antes
+        if ( (nombreEquipo.length() > kTEAMNAME) || (nombreEquipo.length() == 0))
+        {
+            // Se pone el nombre por defecto
            
-            
-
-
-
-
-
-
-
-
-
-
-            for(int j=0;j<kPLAYERNAME;j++){
-                jugador.name[j]=nombre[j];
+            nombreEquipo = "Team_"+equipo.id;
+                       
+            for (int j = 0; j < nombreEquipo.length(); j++)
+            {
+                equipo.name[j] = nombreEquipo[j];
             }
-            jugador.goals=0;
-            jugador.best=false;
-            equipo.players[i]=jugador;
+            
+        }
+        else
+        {
+           
+            for (int j = 0; j < nombreEquipo.length(); j++)
+            {
+                equipo.name[j] = nombreEquipo[j];
+            }
+
         }
 
+*/
 
-    } 
+        equipo.wins = 0;
+        equipo.losses = 0;
+        equipo.draws = 0;
+        equipo.points = 0;
+        
+
+        for (int i = 0; i < kPLAYERS; i++)//bucle añadir jugadores
+        {
+            Player jugador;
+            string nombreJugador = equipo.name;
+            nombreJugador+="-R"+(i+1);
+
+            for (int j = 0; j < nombreJugador.length(); j++){
+                jugador.name[j] = nombreJugador[i];
+            }
+
+            jugador.goals = 0;
+            jugador.best = false;
+            equipo.players[i] = jugador;
+        }
+    }
     return equipo;
 }
-void showTeams() {
-    if (teams.empty()) {
+void showTeams()
+{
+    if (teams.empty())
+    {
         cout << ERR_NO_TEAMS << endl;
         return;
     }
@@ -177,67 +195,81 @@ void showTeams() {
     cout << "Enter team name: ";
     getline(cin, teamName);
 
-    if (teamName.empty()) {// Mostrar información de todos los equipos
-        for (const auto& team : teams) {
+    if (teamName.empty())
+    { // Mostrar información de todos los equipos
+        for (const auto &team : teams)
+        {
             cout << "Name: " << team.name << endl;
             cout << "Wins: " << team.wins << endl;
             cout << "Losses: " << team.losses << endl;
             cout << "Draws: " << team.draws << endl;
             cout << "Points: " << team.points << endl;
-            for (int i = 0; i < team.players.size(); ++i) {
+            for (int i = 0; i < team.players.size(); ++i)
+            {
                 cout << team.name << "-R" << i + 1 << ": " << team.players[i].goals << " goals" << endl;
             }
         }
-    } else {// Buscar el equipo por nombre
+    }
+    else
+    { // Buscar el equipo por nombre
         bool teamFound = false;
-        for (const auto& team : teams) {
-            if (team.name == teamName) {
+        for (const auto &team : teams)
+        {
+            if (team.name == teamName)
+            {
                 teamFound = true;
                 cout << "Name: " << team.name << endl;
                 cout << "Wins: " << team.wins << endl;
                 cout << "Losses: " << team.losses << endl;
                 cout << "Draws: " << team.draws << endl;
                 cout << "Points: " << team.points << endl;
-                for (int i = 0; i < team.players.size(); ++i) {
+                for (int i = 0; i < team.players.size(); ++i)
+                {
                     cout << team.name << "-R" << i + 1 << ": " << team.players[i].goals << " goals" << endl;
                 }
                 break;
             }
         }
-        if (!teamFound) {
+        if (!teamFound)
+        {
             cout << ERR_NOT_EXIST << endl;
         }
     }
 }
 
 // Función principal. Tendrás que añadir más código tuyo
-int main(){
+int main()
+{
     char option;
 
     srand(888); // Fija la semilla del generador de números aleatorios. ¡NO TOCAR!
-    
-    do{
+
+    do
+    {
         showMenu();
         cin >> option;
         cin.ignore(); // Para evitar que el salto de línea se quede en el buffer de teclado y luego pueda dar problemas si usas "getline"
-        
-        switch(option){
-            case '1': // Llamar a la función "addTeam" para añadir un nuevo equipo
-                break;
-            case '2': // Llamar a la función "addAllTeams" para añadir todos los equipos de una vez
-                break;
-            case '3': // Llamar a la función "deleteTeam" para borrar un equipo
-                break;
-            case '4': // Llamar a la función "showTeams" para mostrar los datos de los equipos
-                break;
-            case '5': // Llamar a la función "playLeague" para simular los resultados de la competición
-                break;
-            case '6': // Llamar a la función "showStandings" para mostrar la clasificación final
-                break;
-            case '7': // Llamar a la función "showBestPlayers" para mostrar los mejores jugadores de cada equipo
-                break;
-            case 'q': break;
-            default: error(ERR_OPTION);
+
+        switch (option)
+        {
+        case '1': // Llamar a la función "addTeam" para añadir un nuevo equipo
+            break;
+        case '2': // Llamar a la función "addAllTeams" para añadir todos los equipos de una vez
+            break;
+        case '3': // Llamar a la función "deleteTeam" para borrar un equipo
+            break;
+        case '4': // Llamar a la función "showTeams" para mostrar los datos de los equipos
+            break;
+        case '5': // Llamar a la función "playLeague" para simular los resultados de la competición
+            break;
+        case '6': // Llamar a la función "showStandings" para mostrar la clasificación final
+            break;
+        case '7': // Llamar a la función "showBestPlayers" para mostrar los mejores jugadores de cada equipo
+            break;
+        case 'q':
+            break;
+        default:
+            error(ERR_OPTION);
         }
-    }while(option!='q');
+    } while (option != 'q');
 }
